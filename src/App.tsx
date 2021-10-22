ab88b8c66bed8efe642544d90d9b7e4f821a4fa8
heyang <bertie185@dingtalk.com>
2021-10-22 14:47:37 +0800

    optimized styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Piano from './components/Piano'
+// import MusicScore from './components/MusicScore'
 import './App.less'
 
 const App: React.FC = () => {
@@ -7,6 +8,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Piano/>
+      {/* <MusicScore/> */}
     </div>
   )
 
---
+++ src/components/MusicScore/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.less'
+
+const MusicScore: React.FC = () => {
+
+  return (
+    <div className="music-score">
+      <div className="image"/>
+    </div>
+  )
+
+}
+
+export default MusicScore
