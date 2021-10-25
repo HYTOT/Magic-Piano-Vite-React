77b54c8c5e7a56d115d3569c2ae0643f34aa6f67
heyang <bertie185@dingtalk.com>
2021-10-25 15:08:03 +0800

    optimized codes

--- src/components/Piano/index.tsx
+++ src/components/Piano/index.tsx
@@ -5,26 +5,37 @@ import './index.less'
 const Piano: React.FC = () => {
 
   const [activeKeys, setActiveKeys] = useState<Array<string>>([])
+  // const [playingAudios, setPlayingAudios] = useState<Array<string>>([])
 
+  /** 按下钢琴键 */
+  const addActiveKey = (key: string) => {
+    setActiveKeys(keys => {
+      const tempKeys = [...keys]
+      tempKeys.includes(key) && tempKeys.splice(tempKeys.indexOf(key), 1)
+      tempKeys.push(key)
+      return tempKeys
+    })
+  }
+  /** 松开钢琴键 */
+  const removeActiveKey = (key: string) => {
+    setActiveKeys(keys => {
+      const tempKeys = [...keys]
+      tempKeys.includes(key) && tempKeys.splice(tempKeys.indexOf(key), 1)
+      return tempKeys
+    })
+  }
+  /** 注册键盘按键按下事件 */
   const registerKeyDownHandler = () => {
     document.onkeydown = ({ key }: KeyboardEvent) => {
       if (Constants.PIANO_BUTTONS.includes(key)) {
-        setActiveKeys(keys => {
-          const tempKeys = [...keys]
-          tempKeys.includes(key) && tempKeys.splice(tempKeys.indexOf(key), 1)
-          tempKeys.push(key)
-          return tempKeys
-        })
+        addActiveKey(key)
       }
     }
   }
+  /** 注册键盘按键松开事件 */
   const registerKeyUpHandler = () => {
     document.onkeyup = ({ key }: KeyboardEvent) => {
-      setActiveKeys(keys => {
-        const tempKeys = [...keys]
-        tempKeys.includes(key) && tempKeys.splice(tempKeys.indexOf(key), 1)
-        return tempKeys
-      })
+      removeActiveKey(key)
     }
   }
 
@@ -37,6 +48,7 @@ const Piano: React.FC = () => {
     <div className="piano">
       <div className="buttons">
         {
+          // 钢琴键按钮
           Constants.PIANO_BUTTONS.map((key) => (
             <div
               key={ key }
@@ -48,6 +60,7 @@ const Piano: React.FC = () => {
         }
       </div>
       {
+        // 钢琴音对应 audio 标签
         activeKeys.map(key => {
           const fileName = Constants.PIANO_BUTTONS.indexOf(key) + 1
           return fileName > 0 && fileName <= 21 && (
